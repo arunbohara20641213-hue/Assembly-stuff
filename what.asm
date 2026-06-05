77a7f7b24df89fab1a9a4259728c0d4ff60073be
Arun <arunbohara20641213@gmail.com>
2026-06-05 07:16:13 +0545

    Update what.asm

--- what.asm
+++ what.asm
@@ -18,3 +18,20 @@ JNC DONE
 INR D
 DONE: HLT
 
+
+
+;standard way to convert a two's complement negative number into its positive magnitude absolute negative
+JMP START
+
+;data
+
+;code
+START: NOP
+
+;Start writing your code here
+MVI A, 05H
+SUI 08H
+CMA
+INR A
+HLT
+
